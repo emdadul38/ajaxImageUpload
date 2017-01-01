8d35cf973b32a17b15b2928da70bdde0378c1192
Emdadul Huq <emdadul225@gmail.com>
2017-01-01 09:33:53 +0600

    Image show using javascript

---
+++ image_upload.php
@@ -0,0 +1,22 @@
+<div class="avatar-zone">
+    <img id="img_id" src="<?php echo base_url("upload/existing_studnet_photo/$applicant->STUD_PHOTO"); ?>" alt="select photo" style="width: 180px; height: 160px;"/>
+</div>
+<input type='file' name="photo" onchange="upload_img(this);" class="upload_btn">
+
+<script>
+    $(document).ready(function() {
+        //This function is use for image preview before upload
+        function upload_img(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#img_id').attr('src', e.target.result);
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        
+    });
+</script>
